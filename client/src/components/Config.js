c135ab47c6b015f2fcddd60db491b7e7c6103408
solimleee <newsolim5616@gmail.com>
2021-10-06 23:22:01 +0900

    first commit

---
+++ client/src/components/Config.js
@@ -0,0 +1,7 @@
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
+
+export const API_KEY = 'fae22e9b39d3f5e6b8d58fef9fcf49d1';
